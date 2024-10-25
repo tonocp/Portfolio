509d091d192594b7136c48132ed7a429b05b9b9e
tonocp <amcp88@gmail.com>
2024-10-25 22:00:38 +0200

    feat: added software design section

--- js/type-writer.js
+++ js/type-writer.js
@@ -19,6 +19,7 @@ typewriter
       `&nbsp;&nbsp;&nbsp;&nbsp;${reservada}let</span>${nomVar} backEnd </span> = ` +
       `${string}'Java'</span> + ${string} 'TypeScript' </span> + ${string} 'Python' </span> + ${string}'Spring'</span> + ${string}'NestJS'</span> + ${string} 'FastAPI' </span>;<br/>` +
       `&nbsp;&nbsp;&nbsp;&nbsp;${reservada}let</span> ${nomVar}BBDD</span> = ${string}'MySQL'</span> + ${string}'MongoDB'</span> + ${string}'MS SQL Server'</span>;<br/>` +
+      `&nbsp;&nbsp;&nbsp;&nbsp;${reservada}let</span> ${nomVar}softwareDesign</span> = ${string}'SOLID'</span> + ${string}'Hexagonal Architecture'</span> + ${string}'Microservices'</span> + ${string}'Event Oriented Architecture'</span>;<br/>` +
       `&nbsp;&nbsp;&nbsp;&nbsp;${reservada}let</span> ${nomVar}otherKnowledge</span> = ` +
       `${string}'GIT'</span> + ${string}'Docker'</span> + ${string}'Kubernetes'</span> + ${string}'CI/CD'</span> + ${string}'AWS'</span> + ${string}'Azure'</span> + ${string}'GCP'</span> ;<br/>` +
       `&nbsp;&nbsp;&nbsp;&nbsp;${nomVar}console</span>.${nomFuncion}log</span>${llaves}(</span>${string}'Always learning something new...'</span>${llaves})</span>;<br/> ${llaves}}</span>;`
